dc4f1729081f9b0bfa75d1e63bf334bff32ab242
Warren Wang <warren.wang2020@gmail.com>
2024-06-02 23:10:28 -0700

    Fix beta workflow 4.

--- backend/goose_migrate.sh
+++ backend/goose_migrate.sh
@@ -11,6 +11,30 @@ if [ -e ".env" ]; then
 fi
 
 # Conduct the db up migration for the development db
-GOOSE_MIGRATION_DIR="./sql/schema"
+GOOSE_MIGRATION_DIR="/app/sql/schema"
 GOOSE_DATABASE_URL="postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
-goose -dir "$GOOSE_MIGRATION_DIR" postgres "$GOOSE_DATABASE_URL" $1
+
+# Wait for the PostgreSQL container to be ready
+echo "Waiting for PostgreSQL to start..."
+while ! pg_isready -h postgres -p 5432 -q -U postgres; do
+  sleep 1
+done
+echo "PostgreSQL started"
+
+# Wait for the database to be available
+echo "Waiting for database to be available..."
+until PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -c '\l' | grep $DB_DATABASE > /dev/null; do
+  sleep 1
+done
+echo "Database is available"
+
+echo "Running Goose migrations..."
+goose -dir "$GOOSE_MIGRATION_DIR" postgres "$GOOSE_DATABASE_URL" up
+
+# Check if Goose migrations were successful
+if [ $? -ne 0 ]; then
+  echo "Goose migrations failed"
+  exit 1
+else
+  echo "Goose migrations applied successfully"
+fi
