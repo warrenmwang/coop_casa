8c3ff0a6d46a695e89323f209dce4c3cf4b890e3
Warren Wang <warren.wang2020@gmail.com>
2024-09-28 11:28:55 -0700

    Frontend: update regular dashboard to allow regular users to create communities.

--- frontend/src/components/RegularDashboard.tsx
+++ frontend/src/components/RegularDashboard.tsx
@@ -1,16 +1,58 @@
-import * as React from "react";
+import React, { useState } from "react";
 
 import Title from "./Title";
 import AllLikedEntitiesSection from "./AllLikedEntitiesSection";
+import UserOwnedCommunitiesTable from "./communities/UserOwnedCommunitiesTable";
+import TransferCommunityForm from "./communities/TransferCommunityForm";
+import CreateCommunityForm from "../form/CreateCommunityForm";
+import UpdateCommunityManager from "../form/UpdateCommunityManager";
 
 const RegularDashboard: React.FC = () => {
+  const sections: string[] = ["Your Liked", "Community"];
+  const [currentSection, setCurrentSection] = useState<string>(sections[0]);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setCurrentSection(e.currentTarget.textContent as string);
+  };
+
   return (
     <>
-      <Title
-        title="Your Liked Collections"
-        description="If you're not seeing anything, go browse the users, properties, and communities that exist on Coop now!"
-      />
-      <AllLikedEntitiesSection />
+      {/* Screen Navigation Buttons */}
+      <div className="flex gap-2 my-2 p-2 bg-gray-100 rounded-lg w-fit mx-auto">
+        {sections.map((section, i) => (
+          <button
+            disabled={currentSection === section}
+            key={i}
+            className={`border-2 rounded-md  p-3 ${currentSection === section ? "bg-green-400 hover:bg-none" : "bg-gray-300 hover:bg-gray-400"}`}
+            onClick={handleClick}
+          >
+            {section}
+          </button>
+        ))}
+      </div>
+
+      {currentSection === "Community" && (
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-row flex-wrap justify-center">
+            <UserOwnedCommunitiesTable />
+            <TransferCommunityForm />
+          </div>
+          <div className="flex flex-row flex-wrap justify-center">
+            <CreateCommunityForm />
+            <UpdateCommunityManager />
+          </div>
+        </div>
+      )}
+
+      {currentSection === "Your Liked" && (
+        <>
+          <Title
+            title="Your Liked Collections"
+            description="If you're not seeing anything, go browse the users, properties, and communities that exist on Coop now!"
+          />
+          <AllLikedEntitiesSection />
+        </>
+      )}
     </>
   );
 };
